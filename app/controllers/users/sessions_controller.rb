ff4fa1e92566b2061f63d7da2a4b5098e40fe3c0
John Laxson <jlaxson@mac.com>
2014-03-08 16:35:20 -0700

    Make sure we logout of the openid provider as well

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -10,12 +10,27 @@ class Users::SessionsController < Devise::SessionsController
     set_flash_message :notice, :signed_out if signed_out && is_flashing_format?
     yield resource if block_given?
 
+    if signed_out
+      redirect_path = base_signout_url(root_url + redirect_path.gsub(/^\//, ''))
+    end
+
     # We actually need to hardcode this as Rails default responder doesn't
     # support returning empty response on GET request
     respond_to do |format|
       format.all { head :no_content }
       format.any(*navigational_formats) { redirect_to redirect_path }
     end
+  end
+
+  protected
+
+  def base_signout_url(next_redirect)
+    uri = ENV['ARCDATA_OPENID_URL'] || 'http://localhost:3000'
+    uri = URI.parse uri
+    uri.user = uri.password = nil
+    uri.path = '/openid/logout'
+    uri.query = {post_logout_redirect_uri: next_redirect}.to_query
+    uri.to_s
   end
 
 end
