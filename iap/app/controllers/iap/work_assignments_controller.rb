b01486b7d16d5fb6082bab5382105bd901ae94e2
John Laxson <jlaxson@mac.com>
2014-08-29 10:17:47 -0700

    UI Tweaks to 204s and rendering as pdf

--- iap/app/controllers/iap/work_assignments_controller.rb
+++ iap/app/controllers/iap/work_assignments_controller.rb
@@ -8,6 +8,8 @@ module Iap
     defaults route_prefix: nil
     actions :all, except: :show
     custom_actions resource: [:duplicate], collection: [:print]
+    respond_to :html
+    respond_to :pdf, only: :print
 
     def smart_resource_url
       edit_resource_path
@@ -34,5 +36,10 @@ module Iap
               :special_instructions, :prepared_by_name, :prepared_by_title,
               work_assignment_lines_attributes: [:resource_identifier, :leader, :num_persons, :contact, :reporting_location, :ordinal, :_destroy, :id])]
     end
+
+    def editable
+      true
+    end
+    helper_method :editable
   end
 end
