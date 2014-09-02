e7c453fe2bf3f44b28f5af1128274888318ec497
John Laxson <jlaxson@mac.com>
2014-09-02 15:51:22 -0700

    Plot multiple dsars charts

--- dsars/app/controllers/dsars/charts_controller.rb
+++ dsars/app/controllers/dsars/charts_controller.rb
@@ -3,29 +3,56 @@ module Dsars
     inherit_resources
     belongs_to :environment, finder: :find_by_slug
 
-    def show
-      @incident = params[:incident_id]
-      @line_number = params[:id]
-
-      @report = end_of_association_chain.first
-
-      parent_report = @report
-
-      @line_description = LineDescription.where{(report_version == my{@report.report_version}) & (line_number == my{@line_number})}.first
-      
-      if @line_description.format == 'currency'
-        authorize! Roles::Dsars::FINANCIALS
-      end
-
-      @lines = ReportLine.joins{report}.where{(line_number == my{@line_number}) &
+    class LineInfo
+      attr_reader :parent_report, :line_description, :lines
+      def initialize(parent_report, line_description)
+        @parent_report = parent_report
+        @line_description = line_description
+        @lines = ReportLine.joins{report}.where{(line_number == line_description.line_number) &
                                               (report.incident_number == parent_report.incident_number) &
                                               (report.scope == parent_report.scope) &
                                               (report.unit_id == parent_report.unit_id) &
                                               (report.county_id == parent_report.county_id)
                                               }.order{report.report_number}.includes{report}.to_a
+      end
+
+      def series_data col
+        vals = @lines.map do |line|
+          date = to_js_date(line.report.cob_date)
+          val = line[col].to_json
+          "[#{date}, #{val}]"
+        end.compact
+
+        "[#{vals.join ','}]"
+      end
+
+      def to_js_date(d)
+        "Date.UTC(#{d.year}, #{d.month-1}, #{d.day})"
+      end
+
+      def chart_title
+        "5266 Report #{parent_report.cob_date} - #{line_description.try :name}"
+      end
+    end
+
+    def show
+      @incident = params[:incident_id]
+
+      @report = end_of_association_chain.first
+
+      @line_numbers = params[:id].split ','
+      @lines = @line_numbers.map do |line_no|
+        line_description = LineDescription.where{(report_version == my{@report.report_version}) & (line_number == my{line_no})}.first
+      
+        if line_description.format == 'currency'
+          authorize! Roles::Dsars::FINANCIALS
+        end
+
+        LineInfo.new @report, line_description
+      end
 
       breadcrumb "Report #{@report.report_number}", report_path(parent, @report.report_number)
-      breadcrumb @line_description.name
+      breadcrumb @lines.first.line_description.name
     end
 
     def end_of_association_chain
@@ -35,24 +62,7 @@ module Dsars
 
     private
 
-    helper_method :series_data
-    def series_data col
-      vals = @lines.map do |line|
-        date = to_js_date(line.report.cob_date)
-        val = line[col].to_json
-        "[#{date}, #{val}]"
-      end.compact
-
-      "[#{vals.join ','}]"
-    end
-
     helper_method :chart_title
-    def chart_title
-      "#{parent.name} - 5266 Report #{@report.cob_date} - #{@line_description.try :name}"
-    end
 
-    def to_js_date(d)
-      "Date.UTC(#{d.year}, #{d.month-1}, #{d.day})"
-    end
   end
 end
